d21549f90272950314035e8b49e9a7b10abc7ba8
hammadAuthornate <156046487+hammadAuthornate@users.noreply.github.com>
2025-03-12 14:52:50 +0500

    moved colors to new file

--- app/InvestmentPieChart.tsx
+++ app/InvestmentPieChart.tsx
@@ -13,29 +13,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Card, CardContent } from "@/components/ui/card";
-
-const colors = [
-  "#FF5733", // Vibrant Red
-  "#33FF57", // Fresh Green
-  "#3357FF", // Bright Blue
-  "#FF33A1", // Pink
-  "#FFBD33", // Orange
-  "#A1FF33", // Lime
-  "#33FFF5", // Cyan
-  "#8A33FF", // Purple
-  "#33FF8A", // Mint
-  "#FF3368", // Coral
-  "#3368FF", // Sky Blue
-  "#FF8A33", // Sunset Orange
-  "#FF5733", // Rose Red
-  "#57FF33", // Spring Green
-  "#5733FF", // Indigo
-  "#33A1FF", // Ocean Blue
-  "#F533FF", // Magenta
-  "#33FFBD", // Turquoise
-  "#BD33FF", // Lavender
-  "#3357FF", // Royal Blue
-];
+import { colors } from "@/components/colors";
 
 export default function InvestmentPieChart({
   investmentData,
