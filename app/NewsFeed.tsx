a9ff69a087feaa2e2d1c22181a603ea9c6e849dd
hammadAuthornate <156046487+hammadAuthornate@users.noreply.github.com>
2025-03-14 08:56:14 +0500

    updated NewsFeed component with loading and error states

--- app/NewsFeed.tsx
+++ app/NewsFeed.tsx
@@ -1,51 +1,108 @@
-import { Bell } from 'lucide-react';
+"use client";
+import { Bell, Loader2 } from "lucide-react";
 import { getNews } from "@/app/actions/crypto-panic-action";
 import { formatDistanceToNow } from "date-fns";
-import Link from 'next/link';
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { NewsItem } from "@/lib/crypto-panic";
 
-export default async function NewsFeed() {
-    const response = await getNews();
-    const news = response?.results || []
+export default function NewsFeed() {
+  const [news, setNews] = useState<NewsItem[] | null>(null);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  async function fetchNews() {
+    try {
+      setLoading(true);
+      setError(null);
+      const response = await getNews();
+      const news = response?.results;
+      if (!news) {
+        throw Error("Unable to fetch");
+      }
+      setNews(news);
+    } catch (e) {
+      setError("Unable to fetch the news.");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchNews();
+  }, []);
 
   return (
     <div className={`rounded-xl bg-white overflow-hidden`}>
-        <div className="p-6">
+      <div className="p-6">
         <h2 className="text-lg font-semibold mb-1 flex items-center">
-            <Bell size={18} className="mr-2 text-indigo-500" />
-            Recent News
+          <Bell size={18} className="mr-2 text-indigo-500" />
+          Recent News
         </h2>
-        <p className="text-sm text-opacity-70 mb-6">Latest activities and updates</p>
-        
+        <p className="text-sm text-opacity-70 mb-6">
+          Latest activities and updates
+        </p>
+
         <div className="space-y-6">
-            {news.map((item) => (
-            <Link href={item.url} key={item.id} className={`p-4 rounded-lg bg-gray-50 block hover:bg-gray-100 transition-colors`}>
-                <div className="flex items-start">
+          {isLoading && (
+            <div
+              className="flex items-center justify-center p-8"
+              style={{ width: "100%", height: "80%" }}
+            >
+              <Loader2 className="w-8 h-8 text-primary animate-spin" />
+              <span className="ml-2 text-gray-600">Loading News...</span>
+            </div>
+          )}
+          {error && (
+            <div
+              className="p-4 border-red-500 border rounded-md text-red-500"
+              style={{ width: "100%", height: "auto" }}
+            >
+              {/* <h4 className="mb-2 text-lg font-semibold">
+                Error Loading Content
+              </h4> */}
+              <p>{error}</p>
+            </div>
+          )}
+          {news?.map((item) => (
+            <Link
+              href={item.url}
+              key={item.id}
+              className={`p-4 rounded-lg bg-gray-50 block hover:bg-gray-100 transition-colors`}
+            >
+              <div className="flex items-start">
                 <div className="flex-1 min-w-0">
-                    <div className="flex items-center justify-between mb-1">
-                    <h3 className="text-sm font-medium truncate">{item.source.title}</h3>
-                    <div className='flex gap-1'>
-                    {item.currencies && item.currencies.length > 0 && (
+                  <div className="flex items-center justify-between mb-1">
+                    <h3 className="text-sm font-medium truncate">
+                      {item.source.title}
+                    </h3>
+                    <div className="flex gap-1">
+                      {item.currencies && item.currencies.length > 0 && (
                         <>
-                            {item.currencies.map((currency) => (
-                              <span key={currency.code} className={`text-xs px-2 py-1 rounded-full bg-gray-200 text-gray-600`}>
+                          {item.currencies.map((currency) => (
+                            <span
+                              key={currency.code}
+                              className={`text-xs px-2 py-1 rounded-full bg-gray-200 text-gray-600`}
+                            >
                               {currency.code}
-                             </span>
-                            ))}
+                            </span>
+                          ))}
                         </>
                       )}
                     </div>
-                    
-                    </div>
-                    <p className="text-xs text-opacity-70 mb-2">{item.title}</p>
-                    <div className="flex items-center text-xs text-opacity-50">
-                    <span className='text-xs text-gray-500'>{formatDistanceToNow(new Date(item.published_at))} ago</span>
-                    </div>
+                  </div>
+                  <p className="text-xs text-opacity-70 mb-2">{item.title}</p>
+                  <div className="flex items-center text-xs text-opacity-50">
+                    <span className="text-xs text-gray-500">
+                      {formatDistanceToNow(new Date(item.published_at))} ago
+                    </span>
+                  </div>
                 </div>
-                </div>
+              </div>
             </Link>
-            ))}
+          ))}
         </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
--- app/actions/crypto-panic-action.ts
+++ app/actions/crypto-panic-action.ts
@@ -1,25 +1,22 @@
-// 'use server';
-"use cache";
+"use server";
 import { NewsResponse } from "@/lib/crypto-panic";
 
-// export class CryptoPanic {
-//   static async getNews() {
-export async function getNews(){
-    const API_KEY = "9690b0aac01d5c56bc9344c533be730868c934b0";
-    if (!API_KEY) {
-      throw Error("API_KEY for crypto panic missing ");
-    }
-    const response = await fetch(
-      `https://cryptopanic.com/api/free/v1/posts/?auth_token=${API_KEY}&public=true`,
-      { cache: "force-cache" }
-    );
-    if (response.status === 200) {
-      console.log("fetched news");
-      const data = await response.json();
-      return data as NewsResponse;
-    } else {
-      console.log("error fetching news ", response);
-      //   throw Error("Unable to Fetch a response for the selected address");
-    }
+export async function getNews() {
+  "use cache";
+  const API_KEY = "9690b0aac01d5c56bc9344c533be730868c934b0";
+  if (!API_KEY) {
+    throw Error("API_KEY missing ");
   }
-// }
+  const response = await fetch(
+    `https://cryptopanic.com/api/free/v1/posts/?auth_token=${API_KEY}&public=true`,
+    { cache: "force-cache" },
+  );
+  if (response.status === 200) {
+    console.log("Fetched News");
+    const data = await response.json();
+    return data as NewsResponse;
+  } else {
+    console.log("error fetching news ", response);
+    throw Error("Unable to Fetch News");
+  }
+}
